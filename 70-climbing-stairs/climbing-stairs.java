f21667e9e9eea9864e2a9f322a35c437fbb4cdaf
Bhavani Jagirdar <bhavanijagirdar4@gmail.com>
2026-07-02 15:17:51 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (72.24%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,26 +1,40 @@
 class Solution {
 
-    static int dis(int n,int ans[]){
-        if(n==0)return 1;
-        if(n<0)return 0;
+    // static int dis(int n,int ans[]){
+    //     if(n==0)return 1;
+    //     if(n<0)return 0;
 
-        if(ans[n]!=-1)return ans[n];
+    //     if(ans[n]!=-1)return ans[n];
 
-         int onestep = dis(n-1,ans);
-         int twostep = dis(n-2,ans);
+    //      int onestep = dis(n-1,ans);
+    //      int twostep = dis(n-2,ans);
 
-         return ans[n] = onestep+twostep;
+    //      return ans[n] = onestep+twostep;
     
-    }
+    // }
 
-    public int climbStairs(int n) {
+    // public int climbStairs(int n) {
 
+    //     int ans[]=new int[n+1];
+    //     for(int i=0;i<n+1;i++){
+    //         ans[i]=-1;
+    //     }
+
+    //      return dis(n,ans);
+       
+    // }
+    
+    public int climbStairs(int n){
         int ans[]=new int[n+1];
-        for(int i=0;i<n+1;i++){
-            ans[i]=-1;
+
+        ans[0] = 1;
+        ans[1] = 1;
+
+        for(int i=2;i<n+1;i++){
+            ans[i] = ans[i-1]+ans[i-2];
         }
 
-         return dis(n,ans);
-       
+        return ans[n];
     }
+
 }
